3ac1d75afef7f9258faa97caad7e8d30abb689de
Jmax <jmax407@gmail.com>
2021-05-07 15:51:18 -0400

    contact form enhancements

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,12 +1,34 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
     const [formState, setFormState ] = useState({ name: '', email: '', message: '' });
     const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage ] = useState('');
 
     function handleChange(e) {
+        if(e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid condition statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+              } else {
+                if (!e.target.value.length) {
+                    setErrorMessage(`${e.target.name} is required.`);
+                  } else {
+                    setErrorMessage('something');
+                  }
+              }
+        }
         setFormState({...formState, [e.target.name]: e.target.value })
-    }
+
+        console.log('errorMessage:', errorMessage);
+
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+          }
+    }   
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -19,16 +41,21 @@ function ContactForm() {
           <form id="contact-form" onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="name">Name:</label>
-                <input type="text" name="name" defaultValue={name} onChange={handleChange} />
+                <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
             </div>
             <div>
                 <label htmlFor="email">Email address:</label>
-                <input type="email" name="email" defaultValue={email} onChange={handleChange} />
+                <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
             </div>
             <div>
                 <label htmlFor="message">Message:</label>
-                <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
+                <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
             </div>
+            {errorMessage && (
+            <div>
+                <p className="error-text">{errorMessage}</p>
+            </div>
+            )}
             <button type="submit">Submit</button>
           </form>
         </section>
--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -6,6 +6,14 @@ import Nav from '..';
 
 afterEach(cleanup);
 
+const categories = [
+  { name: 'portraits', description: 'Portraits of people in my life' }
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
+
 describe('Nav component', () => {
   it('renders', () => {
     render(<Nav />);
